7bf2e58de76a72f2ad7a09d194c391b465bd01b5
Haseeb Khan <hk2863262@gmail.com>
2025-07-01 15:15:35 +0500

    Tailwind 'Nativewind' setup: Error Fixes

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
 import { Text, View } from "react-native";
+// import "../global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Welcome!</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-accent font-bold">Welcome!</Text>
+      <Text className="text-blue-500">Welcome!</Text>
     </View>
   );
 }
